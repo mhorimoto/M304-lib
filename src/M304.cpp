109a816ca461810556c7840a4bd095a866e590d4
Masafumim Horimoto on smena8 <masafumi.horimoto@gmail.com>
2023-05-31 03:55:04 +0900

    1st release

---
+++ src/M304.cpp
@@ -0,0 +1,374 @@
+#define _M304_CPP_
+#include <M304.h>
+
+extern char *pgname;
+LiquidCrystal lcd(RS,RW,ENA,DB0,DB1,DB2,DB3,DB4,DB5,DB6,DB7);
+AT24C256      atmem(0);
+EthernetClient client;
+
+stM304 st_m;
+
+void m304Init(void) {
+  int i;
+  Ethernet.init(53);
+  pinMode(SW_ENTER,INPUT_PULLUP);
+  pinMode(SW_UP,INPUT_PULLUP);
+  pinMode(SW_DOWN,INPUT_PULLUP);
+  pinMode(SW_LEFT,INPUT_PULLUP);
+  pinMode(SW_RIGHT,INPUT_PULLUP);
+  pinMode(SW_SAFE,INPUT_PULLUP);
+  pinMode(SW_RLY,INPUT);
+  pinMode(SELECT_VR,INPUT);
+  pinMode(RLY1,OUTPUT);
+  digitalWrite(RLY1,HIGH);
+  pinMode(RLY2,OUTPUT);
+  digitalWrite(RLY2,HIGH);
+  pinMode(RLY3,OUTPUT);
+  digitalWrite(RLY3,HIGH);
+  pinMode(RLY4,OUTPUT);
+  digitalWrite(RLY4,HIGH);
+  pinMode(RLY5,OUTPUT);
+  digitalWrite(RLY5,HIGH);
+  pinMode(RLY6,OUTPUT);
+  digitalWrite(RLY6,HIGH);
+  pinMode(RLY7,OUTPUT);
+  digitalWrite(RLY7,HIGH);
+  pinMode(RLY8,OUTPUT);
+  digitalWrite(RLY8,HIGH);
+  lcd.begin(20,4);
+  lcd.print(pgname);
+  for ( i=0;i<6;i++ ) {
+    st_m.mac[i] = atmem.read(LC_MAC+i);
+  }
+  for ( i=0;i<8;i++ ) {
+    digitalWrite(RLY1+i,LOW);
+  }
+  delay(2000);
+  for ( i=0;i<8;i++ ) {
+    digitalWrite(RLY1+i,HIGH);
+  }
+  
+}
+
+/*********************************/
+/* Cross Key Switch sub-routine  */
+/*********************************/
+struct KYBDMEM crosskey,*ptr_crosskey;
+struct KYBDMEM *getCrossKey(void) {
+  int CancelChattering(int);
+  int e,u,d,l,r,a;
+  e = CancelChattering(SW_ENTER);
+  u = CancelChattering(SW_UP);
+  d = CancelChattering(SW_DOWN);
+  l = CancelChattering(SW_LEFT);
+  r = CancelChattering(SW_RIGHT);
+  a = analogRead(SELECT_VR);
+  // crosskey.kpos = 0;
+  if (e==LOW) crosskey.kpos |= K_ENT;
+  if (u==LOW) crosskey.kpos |= K_UP;
+  if (d==LOW) crosskey.kpos |= K_DOWN;
+  if (l==LOW) crosskey.kpos |= K_LEFT;
+  if (r==LOW) crosskey.kpos |= K_RIGHT;
+  crosskey.selpos = a;
+  ptr_crosskey = &crosskey;
+  return(ptr_crosskey);
+}
+
+int CancelChattering(int s) {
+  unsigned long gauge=0;
+  bool debugMsgFlag(int);
+  gauge=0;
+  while(!digitalRead(s)) {
+    crosskey.longf = false;
+    gauge++;
+  }
+  if ( gauge > PUSH_LONG ) {
+    crosskey.longf = true;
+    if (debugMsgFlag(SO_MSG)) {
+      Serial.print("LONG=");
+      Serial.println(gauge);
+    }
+    return(LOW);
+  }
+  if ( gauge > PUSH_SHORT ) {
+    crosskey.longf = false;
+    if (debugMsgFlag(SO_MSG)) {
+      Serial.print("SHORT=");
+      Serial.println(gauge);
+    }
+    return(LOW);
+  }
+  return(HIGH);
+}
+
+void debug_print(void) {
+  char lbf[80];
+  int i;
+  sprintf(lbf,"MAC:%02X%02X.%02X%02X.%02X%02X",st_m.mac[0],st_m.mac[1],st_m.mac[2],st_m.mac[3],st_m.mac[4],st_m.mac[5]);
+  Serial.println(lbf);
+  Serial.print("IP:");
+  Serial.println(st_m.ip);
+  Serial.print("GW:");
+  Serial.println(st_m.gw);
+  Serial.print("DNS:");
+  Serial.println(st_m.dns);
+}
+
+bool debugMsgFlag(int a) {
+  byte d;
+  d = atmem.read(LC_DBGMSG);
+  if ((d&a)==a) return true;
+  return false;
+}
+
+void LCDd::initWriteArea(int p) {
+  int x,y;
+  if (p >= PAGECNT) return;
+  for(y=0;y<4;y++) {
+    for(x=0;x<20;x++) {
+      LCDd::tarea[p][x][y] = ' ';
+    }
+  }
+}
+
+void LCDd::LineWrite(int p,int y) {
+  int x;
+  if (y>3) return; 
+  if (p >= PAGECNT) return;
+  LCDd::setCursor(0,y);
+  for(x=0;x<20;x++) {
+    LCDd::print(LCDd::tarea[p][x][y]);
+  }
+}
+
+void LCDd::PageWrite(int p) {
+  int y;
+  if (p >= PAGECNT) return;
+  for (y=0;y<4;y++) {
+    LCDd::LineWrite(p,y);
+  }
+}
+
+void LCDd::CharWrite(int p,int x,int y,char a) {
+  if (p >= PAGECNT) return;
+  if (LCDd::setWriteChar(p,x,y,a)) {
+    LCDd::setCursor(x,y);
+    LCDd::print(a);
+  }
+}
+  
+int LCDd::setWriteChar(int p,int x,int y,char a) {
+  if (p >= PAGECNT) return;
+  if ((x<20)&&(y<4)) {
+    LCDd::tarea[p][x][y] = a;
+    return 1;
+  }
+  return 0;
+}
+
+
+int LCDd::setLine(int p,int y,char *a) {
+  int i;
+  if (p>=PAGECNT) return(-1);
+  if ((y<0)||(y>3)) return(-2);
+  i=0;
+  while(*a>0x1f) {
+    LCDd::tarea[p][i][y] = *a;
+    i++;
+    a++;
+    if (i>=20) break;
+  }
+  return(0);
+}
+
+int LCDd::IntRead(int p,int x,int y,int w) {
+  char c[6];
+  int i;
+  if (p>=PAGECNT) return(-1);
+  if ((y<0)||(y>3)) return(-2);
+  if ((w<0)||(w>5)) return(-3);
+  for(i=0;i<w;i++) {
+    c[i] = LCDd::tarea[p][x+i][y];
+  }
+  c[i]=(char)NULL;
+  //  Serial.begin(115200);
+  //  Serial.print("IntRead=");
+  //  Serial.println(c);
+  //  Serial.end();
+  i = atoi(c);
+  return(i);
+}
+
+void LCDd::TextWrite(int p,int x,int y,char a[]) {
+  int i,j;
+  char sv[6],fmt[5];
+  if (p >= PAGECNT) return;
+  i = strlen(a);
+  if ((i+x)>20) i=20-x;
+  if (debugMsgFlag(SO_MSG)) {
+    Serial.print("TextWrite x=");
+    Serial.print(x);
+    Serial.print(",y=");
+    Serial.print(y);
+    Serial.print(",a=");
+    Serial.print(a);
+    Serial.print(",i=");
+    Serial.println(i);
+  }
+  for(j=0;j<i;j++) {
+    LCDd::setWriteChar(p,x+j,y,a[j]);
+  }
+  LCDd::setCursor(x,y);
+  LCDd::print(a);
+  LCDd::setCursor(x,y);
+}
+
+
+void LCDd::IntWrite(int p,int x,int y,int w,bool zp,int a) {
+  int i;
+  char sv[6],fmt[5];
+  //  Serial.begin(115200);
+  if ((w < 1) || (w > 5)) {
+    //    Serial.println("w out of range");
+    //    Serial.end();
+    return;
+  }
+  if (p >= PAGECNT) return;
+  if (zp) {
+    snprintf(fmt,5,"%%0%dd",w);
+  } else {
+    snprintf(fmt,4,"%%%dd",w);
+  }
+  snprintf(sv,w+1,fmt,a);
+  //  Serial.print("IntWrite sv=");
+  //  Serial.println(sv);
+  //  Serial.end();
+
+  for(int i=0;i<w;i++) {
+    LCDd::setWriteChar(p,x+i,y,sv[i]);
+  }
+  LCDd::setCursor(x,y);
+  LCDd::print(sv);
+  LCDd::setCursor(x,y);
+}
+
+char LCDd::CharRead(int p,int x,int y) {
+  char c;
+  if (p >= PAGECNT) return(-1);
+  c = LCDd::tarea[p][x][y];
+  return c;  
+}
+
+int LCDd::getDataInt(int p,int x,int y,int w) {
+  int i,v;
+  char s[6];
+  if ((w>5)||(w<1))  return 0;
+  for(i=0;i<w;i++) {
+    s[i] = LCDd::tarea[p][x+i][y];
+  }
+  s[i]=(char)0;
+  v = int(s);
+}
+
+
+void LCDd::IPWrite(int p,int x,int y,IPAddress ipa) {
+  int w;
+  int ip[4];
+
+  x--;
+  for (byte tb=0; tb<4; tb++) {
+    ip[tb] = (int)((ipa>>(8*tb)) & 0xff);
+    if (ip[tb]<10) {
+      w = 1;
+    } else if (ip[tb]<100) {
+      w = 2;
+    } else {
+      w = 3;
+    }
+    LCDd::IntWrite(p,x+1,y,w,false,ip[tb]);
+    x = x + w + 1;
+    if (tb<3) {
+      LCDd::CharWrite(p,x,y,'.');
+    }
+  }
+}
+
+void LCDd::IPWriteWithCidr(int p,int x,int y,IPAddress ipa,int cidr) {
+  int w;
+  int ip[4];
+
+  x--;
+  for (byte tb=0; tb<4; tb++) {
+    ip[tb] = (int)((ipa>>(8*tb)) & 0xff);
+    if (ip[tb]<10) {
+      w = 1;
+    } else if (ip[tb]<100) {
+      w = 2;
+    } else {
+      w = 3;
+    }
+    LCDd::IntWrite(p,x+1,y,w,false,ip[tb]);
+    x = x + w + 1;
+    if (tb<3) {
+      LCDd::CharWrite(p,x,y,'.');
+    }
+  }
+  LCDd::CharWrite(p,x,y,'/');
+  if (cidr<10) {
+    w = 1;
+  } else {
+    w = 2;
+  }
+  LCDd::IntWrite(p,x+1,y,w,false,cidr);
+}
+
+int mask2cidr(IPAddress nma) {
+  int ci,tb;
+  ci=0;
+  for (tb=0;tb<4;tb++) {
+    //    Serial.begin(115200);
+    //    Serial.print("mask2cidr=");
+    //    Serial.print(nma[tb],DEC);
+    //    Serial.print("  ci=");
+    //    Serial.println(ci);
+    //    Serial.end();
+    switch((nma[tb]&0xff)) {
+    case 128:
+      ci+=1;
+      break;
+    case 192:
+      ci+=2;
+      break;
+    case 224:
+      ci+=3;
+      break;
+    case 240:
+      ci+=4;
+      break;
+    case 248:
+      ci+=5;
+      break;
+    case 252:
+      ci+=6;
+      break;
+    case 254:
+      ci+=7;
+      break;
+    case 255:
+      ci+=8;
+      break;
+    case 0:
+      break;
+    default:
+      return(-2);
+    }
+  }
+  //  Serial.begin(115200);
+  //  Serial.print("return mask2cidr=");
+  //  Serial.println(ci,DEC);
+  //  Serial.end();
+  return(ci);
+}
+
+
+#undef _M304_CPP_
---
+++ src/M304.h
@@ -0,0 +1,184 @@
+#ifndef _M304_H_
+#define _M304_H_
+#define _M304_H_V  110
+
+#include <avr/pgmspace.h>
+#include <LiquidCrystal.h>
+#include <SPI.h>
+#include <Ethernet2.h>
+#include <EthernetUdp2.h>
+#include <EEPROM.h>
+#include <AT24CX.h>
+#include <Wire.h>
+#include <DS1307RTC.h>
+
+/*** COMMON ***/
+
+#define TRUE      1
+#define FALSE     0
+#define ERROR     -1
+#define NO        0
+#define UNMATCH   0
+#define IGNORE    1
+#define CHANGE_MAKE  2
+#define CHANGE_BREAK 3
+
+
+/*** CMODE ***/
+
+#define RUN       0
+#define CMND      1
+
+#define NETCMND  10
+#define RTCCMND  11
+#define SCHCMND  12
+#define EEPROMCMND 13
+
+/*** CMENU ***/
+
+#define NETCONFIG 0
+#define RTCCONFIG 1
+#define SCHCONFIG 2
+#define EEPROMOPE 3
+
+/*** Define for LCD ***/
+#define RS        37
+#define RW        38
+#define ENA       39
+#define DB0       40
+#define DB1       41
+#define DB2       42
+#define DB3       43
+#define DB4       44
+#define DB5       45
+#define DB6       46
+#define DB7       47
+#define PAGECNT    2
+
+/*** Define for Arrow Key ***/
+#define SW_SAFE    3
+#define SW_RLY    31
+#define SW_ENTER  32
+#define SW_UP     33
+#define SW_DOWN   34
+#define SW_LEFT   35
+#define SW_RIGHT  36
+#define SELECT_VR A15
+
+/*** Define Relay Ports ***/
+#define  RLY1    22
+#define  RLY2    23
+#define  RLY3    24
+#define  RLY4    25
+#define  RLY5    26
+#define  RLY6    27
+#define  RLY7    28
+#define  RLY8    29
+
+/*** Ethernet2 Ports ***/
+#define NICSS    53
+
+/*** I2C Address ***/
+#define AT24LC254_ADDR   0x50
+#define AT24LC254_INDEX  0
+
+/*** EEPROM LOWCORE ASSIGN ***/
+#define LC_UECS_ID  0
+#define LC_MAC      6
+#define LC_START    0x20
+#define LC_DBGMSG   0x80  /* bit pos 0x80: Serial out, 0x40: LCD out */
+#define   SO_MSG    0x80
+#define   LCD_MSG   0x40
+#define LC_SCH_START 0x1000
+#define   STHR       0x0
+#define   STMN       0x1
+#define   EDHR       0x2
+#define   EDMN       0x3
+#define   INMN       0x4
+#define   DUMN       0x5
+#define   RLY_L      0xe
+#define   RLY_H      0xf
+#define LC_END      0x7fff
+
+typedef struct stM304 {
+  byte mac[6];
+  boolean dhcpflag=true;
+  byte set_ip[4];
+  IPAddress ip;
+  IPAddress gw;
+  IPAddress dns;
+  IPAddress subnet;
+  int cidr;
+};
+
+/*  Cross Key Switch */
+
+#define K_ENT    1
+#define K_UP     2
+#define K_DOWN   4
+#define K_LEFT   8
+#define K_RIGHT 16
+
+#define K_DIGIT  32
+#define K_XDIGIT 33
+#define K_PRINT  34
+#define K_ALPHA  35
+#define K_ALNUM  36
+
+#define PUSH_SHORT 1000
+#define PUSH_LONG  300000
+
+#ifndef _KYBDMEM_
+#define _KYBDMEM_
+struct KYBDMEM {
+  uint8_t   kpos;
+  uint16_t  selpos;
+  bool      longf;
+};
+#endif
+#ifndef _M304_COMMON_KYBD
+#endif
+
+#ifndef _M304_COMMON_DISPLAY
+#define _M304_COMMON_DISPLAY 1
+
+class LCDd : public LiquidCrystal {
+ private:
+  char tarea[PAGECNT][20][4];
+ public:
+ LCDd():LiquidCrystal(RS,RW,ENA,DB0,DB1,DB2,DB3,DB4,DB5,DB6,DB7) {
+  LiquidCrystal:init(0, RS,RW,ENA,DB0,DB1,DB2,DB3,DB4,DB5,DB6,DB7);
+};
+ LCDd(uint8_t rs, uint8_t enable,
+      uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3,
+      uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7):
+  LiquidCrystal(rs,enable,d0,d1,d2,d3,d4,d5,d6,d7){
+  LiquidCrystal:init(0, rs,255,enable,d0,d1,d2,d3,d4,d5,d6,d7);
+};
+  LCDd(uint8_t rs, uint8_t rw, uint8_t enable,
+       uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3,
+       uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7):
+  LiquidCrystal(rs,rw,enable,d0,d1,d2,d3,d4,d5,d6,d7){};
+  LCDd(uint8_t rs, uint8_t rw, uint8_t enable,
+      uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3);
+  LCDd(uint8_t rs, uint8_t enable,
+      uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3);
+  void initWriteArea(int);
+  int  setWriteChar(int,int,int,char),getDataInt(int,int,int,int),setLine(int,int,char *);
+  int  IntRead(int,int,int,int);
+  char CharRead(int,int,int);
+  void LineWrite(int,int),PageWrite(int),CharWrite(int,int,int,char),IntWrite(int,int,int,int,bool,int);
+  void TextWrite(int,int,int,char[]),IPWrite(int,int,int,IPAddress),IPWriteWithCidr(int,int,int,IPAddress,int);
+};
+
+#endif
+
+#ifndef _M304_CPP_
+    extern void m304Init(void);
+    extern void debug_print(void);
+  #endif
+  extern stM304 st_m;
+  extern LiquidCrystal lcd;
+  extern AT24C256      atmem;
+  extern EthernetClient client;
+#endif
